6f8c8dfc6f2cdb5d765376039bfe0b02a1179d01
David Ragsdale <me@davidjragsdale.com>
2019-12-31 08:18:12 -0500

    Add test for verifying a match between CJS and script

--- angularjs/index.test.js
+++ angularjs/index.test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var fs = require('fs');
 var test = require('../helpers/test').test;
 
 // NOTE: getContext's baseContext must be properly ordered
@@ -6,6 +7,10 @@ var {
   angularJsInjector,
   getContext,
 } = require('./');
+
+test('CJS module matches the script', function() {
+  assert.fail();
+});
 
 test('angularJsInjector is a function', function() {
   assert.equal(typeof angularJsInjector, 'function');
