c8420e1ee944cf68c02a3ffcd8169e303d8bc30b
David Ragsdale <me@davidjragsdale.com>
2019-12-31 10:52:22 -0500

    Remove duplicate functions in angularjs tests

--- angularjs/index.test.js
+++ angularjs/index.test.js
@@ -8,35 +8,7 @@ var angularJsLib = require('./');
 var angularJsInjector = angularJsLib.angularJsInjector;
 var getContext = angularJsLib.getContext;
 
-test('CJS module matches the script', function() {
-  const scriptText = fs.readFileSync(path.join(__dirname, './script.js'), 'UTF-8').trim();
-  const cjsModuleText = fs.readFileSync(path.join(__dirname, './index.js'), 'UTF-8');
-  const moduleExport = 'module.exports = {\n' +
-    '  angularJsInjector: angularJsInjector,\n' +
-    '  getContext: getContext\n' +
-    '};';
-  const cjsWithoutExport = cjsModuleText.replace(moduleExport, '').trim();
-  assert.equal(scriptText, cjsWithoutExport);
-});
-
-test('angularJsInjector is a function', function() {
-  assert.equal(typeof angularJsInjector, 'function');
-});
-
-test('injects a simple dependency', function() {
-  needsParam.$inject = ['TEST_CONSTANT'];
-  function needsParam(param0) {
-    return param0;
-  }
-  var testConstant = 'testValue';
-  var injectedFn = angularJsInjector({
-    TEST_CONSTANT: testConstant
-  }, needsParam);
-  var returnedValue = injectedFn();
-  assert.equal(returnedValue, testConstant);
-})
-
-test('injects dependencies of dependencies', function() {
+function getMockServices() {
   ConstantService.$inject = ['TEST_CONSTANT'];
   function ConstantService(TEST_CONSTANT) {
     function getConstant() {
@@ -67,18 +39,58 @@ test('injects dependencies of dependencies', function() {
     return service.getValue();
   }
 
+  return {
+    ConstantService: ConstantService,
+    DataService: DataService,
+    needsService: needsService
+  }
+}
+
+test('CJS module matches the script', function() {
+  const scriptText = fs.readFileSync(path.join(__dirname, './script.js'), 'UTF-8').trim();
+  const cjsModuleText = fs.readFileSync(path.join(__dirname, './index.js'), 'UTF-8');
+  const moduleExport = 'module.exports = {\n' +
+    '  angularJsInjector: angularJsInjector,\n' +
+    '  getContext: getContext\n' +
+    '};';
+  const cjsWithoutExport = cjsModuleText.replace(moduleExport, '').trim();
+  assert.equal(scriptText, cjsWithoutExport);
+});
+
+test('angularJsInjector is a function', function() {
+  assert.equal(typeof angularJsInjector, 'function');
+});
+
+
+test('injects a simple dependency', function() {
+  needsParam.$inject = ['TEST_CONSTANT'];
+  function needsParam(param0) {
+    return param0;
+  }
+
   var testConstant = 'testValue';
+
+  var injectedFn = angularJsInjector({
+    TEST_CONSTANT: testConstant
+  }, needsParam);
+  var returnedValue = injectedFn();
+  assert.equal(returnedValue, testConstant);
+})
+
+test('injects dependencies of dependencies', function () {
+  var mockServices = getMockServices();
+
+  var testConstant = 'testValue';
+
   var injectedConstantService = angularJsInjector({
     TEST_CONSTANT: testConstant
-  }, ConstantService);
+  }, mockServices.ConstantService);
   var injectedDependency = angularJsInjector({
     ConstantService: injectedConstantService,
-  }, DataService);
-  // var whatever = new injectedDependency();
-  // console.log('TESTING', whatever.getValue());
+  }, mockServices.DataService);
   var injectedFn = angularJsInjector({
     DataService: injectedDependency
-  }, needsService);
+  }, mockServices.needsService);
   var returnedValue = injectedFn();
   assert.equal(returnedValue, testConstant);
 });
@@ -87,48 +99,17 @@ test('getContext is a function', function() {
   assert.equal(typeof getContext, 'function');
 });
 
-ConstantService.$inject = ['TEST_CONSTANT'];
-
-function ConstantService(TEST_CONSTANT) {
-  function getConstant() {
-    return TEST_CONSTANT;
-  }
-
-  return {
-    getConstant: getConstant
-  }
-}
-
-DataService.$inject = ['ConstantService'];
-
-function DataService(Service) {
-  var nestedService = new Service();
-
-  function getValue() {
-    return nestedService.getConstant();
-  }
-
-  return {
-    getValue: getValue
-  };
-}
-
-needsService.$inject = ['DataService'];
-
-function needsService(Service) {
-  const service = new Service();
-  return service.getValue();
-}
-
-var testConstant = 'testValue';
-
 test('getContext nests dependencies when in proper order', function() {
+  var mockServices = getMockServices();
+
+  var testConstant = 'testValue';
+
   // Desired output context without injection
   var baseContext = {
     TEST_CONSTANT: testConstant,
-    ConstantService: ConstantService,
-    DataService: DataService,
-    needsService: needsService
+    ConstantService: mockServices.ConstantService,
+    DataService: mockServices.DataService,
+    needsService: mockServices.needsService
   };
 
   var ctx = getContext(baseContext);
@@ -138,11 +119,15 @@ test('getContext nests dependencies when in proper order', function() {
 });
 
 test('getContext errors when in improper order', function () {
+  var mockServices = getMockServices();
+
+  var testConstant = 'testValue';
+
   // Desired output context without injection
   var baseContext = {
-    needsService: needsService,
-    DataService: DataService,
-    ConstantService: ConstantService,
+    needsService: mockServices.needsService,
+    DataService: mockServices.DataService,
+    ConstantService: mockServices.ConstantService,
     TEST_CONSTANT: testConstant
   };
 
